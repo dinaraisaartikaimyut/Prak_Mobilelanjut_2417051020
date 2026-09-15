7873c7c65ae964383db44b71c7c685735acb2e2b
dinaraisaartikaimyut <dinaraisaartika@gmail.com>
2026-09-15 10:07:51 +0700

    membuat form interaktif pertemuan4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'column_widget.dart';
-// import 'row_widget.dart';
+import 'form_widget.dart';
 import 'first_widget.dart';
 
 void main() {
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const FirstWidget(),
+      home: const FormWidget(),
     );
   }
 }
